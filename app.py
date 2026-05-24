70ca828d9919f61798a67cc02bcec33fa8c856ef
walaa-code <walaabourkhis@gmail.com>
2026-05-24 23:03:06 +0100

    add db port

--- app.py
+++ app.py
@@ -31,10 +31,10 @@ socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
 otp_storage = {}
 chat_rooms  = {}
-
 def get_db_connection():
     return pymysql.connect(
         host=os.getenv('DB_HOST', 'localhost'),
+        port=int(os.getenv('DB_PORT', 3306)),
         user=os.getenv('DB_USER', 'root'),
         password=os.getenv('DB_PASSWORD', ''),
         database=os.getenv('DB_NAME', 'users_db'),
